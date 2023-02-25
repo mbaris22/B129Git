cdf261d04fc5f7c26cb826380195e02f6318f925
Mustafa Baris <mbariss68@gmail.com>
2023-02-25 16:54:21 +0300

    V4

--- src/main/java/Toplama.java
+++ src/main/java/Toplama.java
@@ -1,15 +1,17 @@
 public class Toplama {
 
     public static void main(String[] args) {
-    topla(3,2,5,-10);
-    //v3 techpro calismalari
+        topla(3, 2, 5, -10);
+        //v3 techpro calismalari
+        //V4 techpro kodlari..
     }
-    public static void topla(int... a){
-       int toplam=0;
-        for(int w:a){
-           toplam+=w;
 
-       }
+    public static void topla(int... a) {
+        int toplam = 0;
+        for (int w : a) {
+            toplam += w;
+
+        }
         System.out.println("toplam = " + toplam);
     }
 }
